41aefdb278eec0a55decab90f417431bf3b6a57f
Annika <36318224+annikabhatia@users.noreply.github.com>
2026-07-22 22:02:35 -0400

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # BRUTE FORCE WAY
+        # have a for loop that goes through each element
+        # set left to that element, and have right iterate until you find a value that is larger than the left
+        # find the difference between (index of right) - (index of left), and assign that to count
+        # replace the element with the count
+
+        # MONOTONIC DECREASING STACK    
+        
+        res = [0] * len(temperatures)
+        stack = [] # pair: [temp,index]
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = (i - stackInd)
+            stack.append([t, i])
+        return res
+
+        
